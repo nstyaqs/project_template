4a891cfe5223bbebc4cf6fc5a96c891fa0dd0abe
Anastasiia Sych <a.sych@ukma.edu.ua>
2025-03-31 18:10:06 +0300

    Added empty functions for input and output operations

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def input_text():
+    pass
+
+def read_file_builtin():
+    pass
+
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def output_text():
+    pass
+
+def write_file_builtin():
+    pass
+
+def write_file_pandas():
+    pass
